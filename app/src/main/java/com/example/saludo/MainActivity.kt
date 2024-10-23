b8a368cb52cdad758a25e38951ab5ce3239993ed
davsanper2 <sankeyr3@gmail.com>
2024-10-23 09:52:13 +0200

    trabajo terminado

---
+++ app/src/main/java/com/example/saludo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.saludo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        val etName:AppCompatEditText = findViewById(R.id.etName)
+        btnHello.setOnClickListener{
+
+            val name:String = etName.text.toString()
+            val intentGA = Intent(this, Activity2::class.java)
+            intentGA.putExtra("EXTRA_NAME", name)
+            startActivity(intentGA)
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
